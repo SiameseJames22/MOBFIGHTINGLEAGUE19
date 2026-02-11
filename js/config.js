8b8458d16ffa2ee54067b9a3aa1dee8997d7378a
SiameseJames22 <robloxworld607@gmail.com>
2026-02-11 20:26:33 +0000

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,11 +1,10 @@
-// EDIT THIS FILE (only once) ✅
 export const firebaseConfig = {
   apiKey: "AIzaSyDy-Q9XJSuyvJkuahYpDy8cDOhe7PUzdGw",
   authDomain: "mobfightingleague.firebaseapp.com",
   projectId: "mobfightingleague",
-  storageBucket: "mobfightingleague.firebasestorage.app",
+  storageBucket: "mobfightingleague.appspot.com", // FIXED
   messagingSenderId: "495866118126",
   appId: "1:495866118126:web:dbc03a8c89bcd2c01118ff"
 };
 
-export const ADMIN_UID = "PASTE_YOUR_UID_HERE";
+export const ADMIN_UID = "TDEzUkYzihZ6rVuE6lGpUuFlatM2";
